4672b5726edf97db20e1c1be36b79c6d8a53ad16
Chieko Sato <chiekosato.02@gmail.com>
2023-04-05 16:49:05 +0900

    remove h1 from html

---
+++ workshop/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+// const App = () => {
+//   return <h1>This is the app </h1>
+// }
+
+// import React from 'react';
+
+const App = () => {
+  return (
+    <div class Name= "left-scene">
+      <SrerchBar />
+    </div>
+  );
+};
+
+export default App;
+export {App};
---
+++ workshop/src/components/Gif.jsx
@@ -0,0 +1,13 @@
+// rsc functinal compornents
+//  rcc
+import React from 'react';
+
+const Gif = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Gif;
